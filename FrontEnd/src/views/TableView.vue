4f268c6c217c44fef8a631cfc03c20f465a18072
Llllrf <1269957412@qq.com>
2021-12-09 13:36:31 +0800

    display the data

--- FrontEnd/src/views/TableView.vue
+++ FrontEnd/src/views/TableView.vue
@@ -5,9 +5,19 @@
         :transform="'translate(0,'+ rowindex * cellHeight +')'">
         <rect v-for="(column, columnindex) in row" 
           class="table-cell"
-          :x="columnindex*cellWidth" :width="cellWidth"
-          y="0" :height="cellHeight">
+          :x="cal_x(row, columnindex)"
+          y="0" 
+          :width="cal_width(row, columnindex)"
+          :height="cellHeight">
         </rect>
+        <text v-for="(column, columnindex) in row" 
+          v-if="column.value != 'None'"
+          class="table-cell-text"
+          :x="cal_x(row, columnindex)" :y="textY"
+          xml:space="preserve"
+          >
+          {{column.value}}
+        </text>
       </g>
     </svg>
   </div>
@@ -22,21 +32,52 @@ export default {
   },
   data() {
     return {
-      cellWidth: 40,
-      cellHeight: 20,
-      tabularDatasetList: sysDatasetObj.tabularDatasetList
+      cellWidth: 50,
+      cellHeight: 25,
+      textX: 5,
+      textY: 20,
+      tabularDatasetList: null
+    }
+  },
+  methods: {
+    ...mapMutations([
+        'UPDATE_DISPLAY_MODE',
+        'UPDATE_COLUMN_WIDTH_LIST'
+    ]),
+    cal_x(row, index) {
+      var padding = 10;
+      var res = 0;
+      for (var i=0; i<index; i++) {
+        res = res + row[i].length;
+      }
+      return res * padding;
+    },
+
+    cal_width(row, index) {
+      var padding = 10;
+      return row[index].length * padding;
     }
   },
   watch: {
       displayMode: function() {
       }
   },
+  beforeMount: function() {
+    this.tabularDatasetList = sysDatasetObj.tabularDatasetList
+
+  },
   mounted: function() {
     console.log('this.tabularDatasetList', this.tabularDatasetList)
+    console.log('before',this.displayMode)
+    this.UPDATE_DISPLAY_MODE('hi')
+    console.log('after',this.displayMode)
+    console.log('this.columnWidthList', this.columnWidthList)
+
   },
   computed: {
     ...mapState([
-        'displayMode'
+        'displayMode',
+        'columnWidthList'
       ])
   }
 }
@@ -51,8 +92,12 @@ export default {
     height: 100%;
     width: 100%;
     .table-cell {
-      fill: #eeeeee;
-      stroke: white;
+      fill: white;
+      stroke: #eeeeee;
+      stroke-width: 0.5px;
+    }
+    .table-cell-text{
+      text-anchor: middle;
     }
   }
 }
