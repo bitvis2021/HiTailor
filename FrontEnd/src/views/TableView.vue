38ac91b9692c8bc61c9f47daa567e054e9c81a16
Llllrf <1269957412@qq.com>
2021-12-11 23:37:03 +0800

    add a very inelegant implementation of selecting cells

--- FrontEnd/src/views/TableView.vue
+++ FrontEnd/src/views/TableView.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="table-view">
     <button class="button" id="header-button"
-      @click="" > 
+        draggable
+        @dragstart="handle_drag()"
+        @drag="handle_drag()"
+        @dragend="handle_drag()" > 
       header 
     </button>
     
@@ -50,30 +53,41 @@
         :transform="'translate(' + cellWidth + ',' + (cal_range(0, rowindex, rowHeightList) + cellHeight) + ')'">
         <rect v-for="(column, columnindex) in row" :key="column.index"
           class="table-cell"
+          :ref="'cell(' + rowindex + ',' + columnindex +')' "
           :x="cal_range(0, column.start, columnWidthList)"
           y="0" 
           :width="cal_range(column.start, column.end+1, columnWidthList)"
           :height="rowHeightList[rowindex]"
-          @click="change_selected_cell(rowindex, columnindex)"
+          @mousedown="change_selected_cell(rowindex, columnindex)"
+          @mouseover="handle_mouse_over(rowindex, columnindex, $event)"
           >
         </rect>
         <text v-for="(column, columnindex) in row" :key="column.index"
           v-if="column.value != 'None'"
           class="table-cell-text"
+          :ref="'cellText(' + rowindex + ',' + columnindex +')' "
           :x="cal_range(0, column.start, columnWidthList) + textPaddingX" :y="textPaddingY"
-          @click="change_selected_cell(rowindex, columnindex)"
+          @mousedown="change_selected_cell(rowindex, columnindex)"
+          @mouseover="handle_mouse_over(rowindex, columnindex, $event)"
           >
             {{get_substring(column.value, column.start, column.end, columnindex, row)}}
         </text>
       </g>
 
       <!-- selected cell -->
-      <rect class="selected-cell"
-        :x="cellWidth + cal_range(0, tabularDatasetList[selectedCell.row][selectedCell.column].start, columnWidthList)" 
-        :y="cellHeight + cal_range(0, selectedCell.row, rowHeightList)" 
-        :width="cal_range(tabularDatasetList[selectedCell.row][selectedCell.column].start, tabularDatasetList[selectedCell.row][selectedCell.column].end+1, columnWidthList)"
-        :height="rowHeightList[selectedCell.row]">
+      <rect class="selected-area"
+        :x="cellWidth + cal_range(0, tabularDatasetList[selectedArea.top][selectedArea.left].start, columnWidthList)" 
+        :y="cellHeight + cal_range(0, selectedArea.top, rowHeightList)" 
+        :width="cal_range(tabularDatasetList[selectedArea.top][selectedArea.left].start, tabularDatasetList[selectedArea.bottom][selectedArea.right].end + 1, columnWidthList)"
+        :height="cal_range(selectedArea.top, selectedArea.bottom + 1, rowHeightList)"
+        @mouseup="handle_mouse_up()"
+        draggable
+        @dragstart="handle_drag()"
+        @drag="handle_drag()"
+        @dragend="handle_drag()"
+        >
       </rect>
+
     </svg>
   </div>
 </template>
@@ -96,8 +110,11 @@ export default {
       tabularDatasetList: null,
       columnWidthList: null,
       rowHeightList: null,
-
-      selectedCell: {row:0, column:0}
+      
+      selectedCell: {row:0, column:0},
+      selectedArea: {top:0, left:0, bottom:0, right:0},
+      mouserOverCell: {row:null, column:null}
+      
     }
   },
 
@@ -132,7 +149,7 @@ export default {
       }
       else if ( index+1 < row.length && row[index+1].value=='None')   // the cell after it is none
       {
-        cellLength += this.cal_range(row[index+1].start, row[index+1].end+1, this.columnWidthList)
+        cellLength += this.cal_range(row[index+1].start, row[index+1].end+1, this.columnWidthList) - this.textPaddingX
         if (textLength < cellLength) {
           return text
         }
@@ -152,7 +169,7 @@ export default {
     get_text_width(t) {  
       var text = document.createElement("span")
       document.body.appendChild(text)
-      text.style.font = "Lucida";
+      text.style.fontFamily = "Helvetica, Tahoma, Arial";
       text.style.fontSize = "100%";
       text.style.height = 'auto';
       text.style.width = 'auto';
@@ -175,19 +192,61 @@ export default {
       }
     },
 
+    // check_cell_is_selected(row, column) {
+    //   if (row == this.selectedArea.top && column == this.selectedArea.column) {
+    //     return true
+    //   }
+    //   else {
+    //     return false
+    //   }
+    // },
 
-    check_cell_is_selected(row, column) {
-      if (row == this.selectedCell.row && column == this.selectedCell.column) {
-        return true
-      }
-      else {
-        return false
-      }
+    change_selected_cell(row, column) {
+      this.selectedCell.row = row
+      this.selectedCell.column = column
+
+      this.selectedArea.top = row
+      this.selectedArea.left = column
+      this.selectedArea.bottom = row
+      this.selectedArea.right = column
+
+      window.onmousemove = this.handle_mouse_move
     },
 
-    change_selected_cell(row, column){
-      this.selectedCell = {row, column}
-    }
+    handle_mouse_move(event) {
+      if (this.selectedCell.row <= this.mouserOverCell.row && this.selectedCell.column <= this.mouserOverCell.column) {
+        this.selectedArea.bottom = this.mouserOverCell.row
+        this.selectedArea.right = this.mouserOverCell.column
+      }
+      else {
+        this.selectedArea.bottom = this.selectedCell.row
+        this.selectedArea.right = this.selectedCell.column
+        this.selectedArea.top = this.mouserOverCell.row
+        this.selectedArea.left = this.mouserOverCell.column
+
+        console.log("selectedArea.bottom", this.selectedArea.bottom)
+        console.log("selectedArea.right", this.selectedArea.right)
+        console.log("selectedArea.top",this.selectedArea.top )
+        console.log("selectedArea.left",this.selectedArea.left )
+      }
+      
+      // console.log("mousemove", this.selectedArea.bottom, this.selectedArea.right)
+    },
+
+    handle_mouse_over(row, column) {
+      this.mouserOverCell.row = row
+      this.mouserOverCell.column = column
+      // console.log("mouseover", row, column)
+    },
+
+    handle_mouse_up (event) {
+      window.onmousemove = null
+      // event.currentTarget.style.cursor = 'move'
+    },
+
+    handle_drag() {
+      console.log("ininin")
+    },
   },
 
   watch: {
@@ -241,7 +300,7 @@ export default {
   computed: {
     ...mapState([
         'displayMode'
-      ])   
+      ])
   }
 }
 </script>
@@ -265,7 +324,7 @@ export default {
       stroke-width: 0.2px;
     }
     .table-mark-text {
-      font-family: "Lucida";
+      font-family: Helvetica, Tahoma, Arial;
       fill:black;
       font-size:105%;
       text-anchor: middle;
@@ -276,12 +335,13 @@ export default {
       stroke-width: 0.2px;
     }
     .table-cell-text {
-      font-family: "Lucida";
+      font-family: Helvetica, Tahoma, Arial;
       text-anchor: start;
     }
-    .selected-cell {
+    .selected-area {
       stroke: steelblue;
-      fill: none;
+      fill: steelblue;
+      fill-opacity: 10%;
       stroke-width: 1px;
     }
       
