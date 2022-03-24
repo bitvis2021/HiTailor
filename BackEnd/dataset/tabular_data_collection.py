c60391d1bdf95b521621b5fa4ffa29a1a69fcd08
Llllrf <1269957412@qq.com>
2022-03-24 23:33:10 +0800

    change data to table2

--- BackEnd/dataset/tabular_data_collection.py
+++ BackEnd/dataset/tabular_data_collection.py
@@ -8,12 +8,12 @@ def load_tabular_dataset():
         read tabular dataset and process
     '''
     global tabular_dataset_list
-    s = parse_sheet("public/table1.xlsx", "Sheet1")
+    s = parse_sheet("public/table2.xlsx", "Sheet1")
     tabular_data_content = s.result()
     print(type(tabular_data_content))
     print(str(tabular_data_content))
     tabular_data_obj = {}
-    tabular_data_obj["filename"] = "table1.xlsx"
+    tabular_data_obj["filename"] = "table2.xlsx"
     tabular_data_obj["row"] = 10
     tabular_data_obj["column"] = 20
     tabular_data_obj["content"] = str(tabular_data_content)
