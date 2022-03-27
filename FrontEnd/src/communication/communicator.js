b19272c7273b0424f9182b7fee8571e54ca66844
I-Rinka <I_Rinka@outlook.com>
2022-03-27 16:55:02 +0800

    delete useless things

--- FrontEnd/src/communication/communicator.js
+++ FrontEnd/src/communication/communicator.js
@@ -1,6 +1,7 @@
 // 
 import axios from 'axios'
-let server_address = 'http://127.0.0.1:14450'
+// let server_address = 'http://127.0.0.1:14450'
+let server_address = 'http://123.57.216.53/hitailor'
 
 export function getTabularDataset(tabularDataList, getTabularDataCallback) {
     let formData = {"tabularData": tabularDataList}
